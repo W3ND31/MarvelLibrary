14fc340fb243240ddad3119c0b776caaf8f99440
Wendel <wendelmacedooliveira@hotmail.com>
2019-07-22 16:46:02 -0300

    Ajeitando o Rest

--- Data/SeedingCharacters.cs
+++ Data/SeedingCharacters.cs
@@ -1,9 +1,8 @@
 ﻿using MarvelLibrary.Models;
 using RestSharp;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+using MarvelLibrary.Utilities;
 
 namespace MarvelLibrary.Data
 {
@@ -19,6 +18,13 @@ namespace MarvelLibrary.Data
         public void Seed()
         {
             string apiKey = "8a67731f0dd52a1b7260c3f159b92e2b";
+            DateTime ts = DateTime.Now;
+            string privateKey = "2d9b34399dd38c7ced59ab126c7d74b1e51efc5d";
+            string hash;
+            string input = $"{ts}{privateKey}{apiKey}";
+            MD5 md5 = MD5.Create();
+            hash = Md5Hash.GetMd5Hash(md5, input);
+            
             var client = new RestClient("https://gateway.marvel.com:443/v1/public");
             var request = new RestRequest($"/characters?apikey={apiKey}");
             var response = client.Get(request);
